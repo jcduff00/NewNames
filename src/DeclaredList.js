8fc8fe37d9261144faf8e19fd89ddfbc64cadadb
jcduff00 <johncduffman@gmail.com>
2023-02-23 17:37:54 -0500

    imported json for housing data

--- src/DeclaredList.js
+++ src/DeclaredList.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import './DeclaredList.css';
 import DeclaredSpace from './DeclaredSpace'
-
+import data from './declared_data.json'
 
 function DeclaredList() {
+
+    const spaces = data.map((obj) => {
+        const { name, party, images } = obj
+      return (
+        <DeclaredSpace
+          key={name}
+          name={name}
+          party={party}
+          image={images[0]}
+        />
+      )
+    })
     return (
       <div className="DeclaredList">
-        <DeclaredSpace 
-            name="Marianne Williamson"
-            party="Democratic"
-            image="Marianne_Williamson.jpg"
-            />
-        <DeclaredSpace 
-            name="Nikki Haley"
-            party="Republican"
-            image="Nikki_Haley.jpg"/>
-        <DeclaredSpace 
-            name="Vivek Ramaswamy"
-            party="Republican"
-            image="Vivek.jpg"/>
+        { spaces }
       </div>
     )
 }
+
+// 
+// function DeclaredList() {
+//     return (
+//       <div className="DeclaredList">
+//         <DeclaredSpace 
+//             name="Marianne Williamson"
+//             party="Democratic"
+//             image="Marianne_Williamson.jpg"
+//             />
+//         <DeclaredSpace 
+//             name="Nikki Haley"
+//             party="Republican"
+//             image="Nikki_Haley.jpg"/>
+//         <DeclaredSpace 
+//             name="Vivek Ramaswamy"
+//             party="Republican"
+//             image="Vivek.jpg"/>
+//       </div>
+//     )
+// }
   
 
 export default DeclaredList
